a8e4ae73e2a4feb58f7c8af458a53c5fbe42c7eb
madhurima-beryl <madhurima.chhabra@berylsystems.com>
2018-04-30 13:05:11 +0530

    created resources for all entities

--- rails/Hospital/config/routes.rb
+++ rails/Hospital/config/routes.rb
@@ -2,35 +2,21 @@ Rails.application.routes.draw do
   get 'homepage/index'
   root 'homepage#index'
   get 'doctors/main', to: 'doctors#main'
-  resources :doctors
-#  get 'appointments/index' 
-#  get 'appointments/show'
-#  get 'appointments/new'
-#  get 'appointments/edit'
-#  get 'appointments/create'
-#  get 'appointments/update'
-#  get 'appointments/destroy'
-#  get 'specializations/index'
-#  get 'specializations/show'
-#  get 'specializations/new'
-#  get 'specializations/edit'
-#  get 'specializations/create'
-#  get 'specializations/update'
-#  get 'specializations/destroy'
-#  get 'patients/index'
-#  get 'patients/show'
-#  get 'patients/new'
-#  get 'patients/edit'
-#  get 'patients/create'
-#  get 'patients/update'
-#  get 'patients/destroy'
-  get 'doctors/new', to: 'doctors#new'
-  get 'doctors/:id', to: 'doctors#show'
-  get 'doctors', to: 'doctors#index' 
-  get 'doctors/:id/edit', to: 'doctors#edit'
-  post 'doctors', to: 'doctors#create'
-#  get 'doctors/update'
-#  get 'doctors/destroy'
+  resources :doctors, :patients, :specializations, :appointments
+
+
+######### DOCTORS NON RESOURCEFUL ROUTES #########
+
+#  get 'doctors/new', to: 'doctors#new'
+#  get 'doctors/:id', to: 'doctors#show', as: :doctor
+#  get 'doctors', to: 'doctors#index', as: :doctors 
+#  get 'doctors/:id/edit', to: 'doctors#edit', as: :edit_doctor
+#  post 'doctors', to: 'doctors#create'
+#  patch 'doctors/:id', action: :update, controller: :doctors, id: :doctor
+#      patch 'doctors/:id', to: 'doctor#update', id: :doctor
+#      put 'doctors/:id', to: 'doctor#update'
+#      match '/doctors/:id' => redirect('/doctors/%{id}'), via: [:patch]
+#  delete 'doctors/:id', action: :destroy, controller: :doctors, id: :doctor
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
