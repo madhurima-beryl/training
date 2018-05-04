6db1486bf035d02a30747527de08834f8d2c0753
madhurima-beryl <madhurima.chhabra@berylsystems.com>
2018-05-04 18:09:14 +0530

    created simple devise application

---
+++ rails/homedevise/app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def toc
+  end 
+end
---
+++ rails/homedevise/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
+
+  # attr_accessible :image, :remote_image_url
+  mount_uploader :image, ImageUploader
+end
