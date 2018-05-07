b51f9b06b0f1a089af1373de632f4af3aa8d57ac
madhurima-beryl <madhurima.chhabra@berylsystems.com>
2018-05-07 12:40:59 +0530

    created facebook authentication

--- rails/homedevise/app/models/user.rb
+++ rails/homedevise/app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
+         :confirmable, :lockable, :timeoutable,
+        :omniauthable, omniauth_providers: [:facebook, :google_oauth2,]
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.skip_confirmation!
+    end
 
   # attr_accessible :image, :remote_image_url
   mount_uploader :image, ImageUploader
