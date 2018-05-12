9d2b8f94aa0db2b4f135f788d8a0b5d6ca3797cc
madhurima-beryl <madhurima.chhabra@berylsystems.com>
2018-05-12 16:08:45 +0530

    customised item serializer for todo

--- rails/todolist_api/app/serializers/item_serializer.rb
+++ rails/todolist_api/app/serializers/item_serializer.rb
@@ -1,4 +1,18 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :done
-  belongs_to :todo
+  belongs_to :todo#, serializer: TodoSerializer, exclude: :id
+
+  def todo
+    # object.todo.where(created_by: scope)
+    {
+      title: object.todo.title ,
+      assigned_by: object.todo.created_by
+    }
+  end
 end
+
+# def todo
+   
+#   	@todo.title = "#{object.todo.title}"
+#   	@todo.created_by = "#{object.todo.created_by}"   
+#   end
